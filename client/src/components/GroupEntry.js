f2de49cc6f81b8c79c3142d29dcd0af877fdef87
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-03 18:49:47 +0200

    New components for Group/Event

---
+++ client/src/components/GroupEntry.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GroupEntry() {
+  return <div>GroupEntry</div>;
+}
+
+export default GroupEntry;
---
+++ client/src/components/GroupModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GroupModal() {
+  return <div>GroupModal</div>;
+}
+
+export default GroupModal;
