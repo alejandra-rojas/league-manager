94077c829532b7f0c4d40ce7dfb02ee3d6a27c4b
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-03 10:56:48 +0200

    Database structure changes

--- server/data.sql
+++ server/data.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE leaguesdata;
+CREATE DATABASE leaguemgmt;
 
 CREATE TABLE admin_users (
     admin_email VARCHAR (255) PRIMARY KEY,
@@ -12,21 +12,21 @@ CREATE TABLE leagues (
     starting_date VARCHAR(10),
     midway_point VARCHAR(10),
     end_date VARCHAR(10),
-    league_events text ARRAY,
-    isFinished BOOLEAN
+    league_events INTEGER[],
+    isFinished BOOLEAN DEFAULT false
 );
 
 INSERT INTO leagues(league_name, starting_date, midway_point, end_date) VALUES ('WOMENS DOUBLES', '2023-08-01', '2023-08-15', '2023-08-29');
 
 
 CREATE TABLE events (
-    id VARCHAR (255) PRIMARY KEY,
+    event_id SERIAL PRIMARY KEY,
     event_name VARCHAR(40),
     participating_teams 
 );
 
 CREATE TABLE teams (
-    id VARCHAR (255) PRIMARY KEY,
+    team_id SERIAL PRIMARY KEY,
     player1_fullname VARCHAR(40),
     player2_fullname VARCHAR (40),
     players_emails integer ARRAY[2],
