9ef6c5b2664f801718bb5eaccf8258154cd48063
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-03 08:16:01 +0200

    Added Auth signout button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
+import { useCookies } from "react-cookie";
 
 export default function Header({ getData }) {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const adminEmail = cookies.AdminEmail;
   const [showModal, setShowModal] = useState(false);
+
+  const signOut = () => {
+    console.log("loged out");
+    removeCookie("AdminEmail");
+    removeCookie("AuthToken");
+    window.location.reload();
+  };
 
   return (
     <div>
-      <div className="inline-flex">
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-          Login
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
-          onClick={() => setShowModal(true)}
-        >
-          Create League
-        </button>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
-          Sign Out
-        </button>
+      <div className="flex justify-between">
+        <p>Welcome back {adminEmail}</p>
+        <div className="inline-flex">
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+            onClick={() => setShowModal(true)}
+          >
+            Create League
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
       </div>
       {showModal && (
         <Modal mode={"create"} setShowModal={setShowModal} getData={getData} />
