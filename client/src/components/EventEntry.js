f336fd2e5c5bc9d0949eef8736e69a96e2f5e13a
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-07 15:40:19 +0200

    Added search input for players and teams

--- client/src/components/EventEntry.js
+++ client/src/components/EventEntry.js
@@ -5,7 +5,9 @@ function EventEntry({ gevent, getEventsData }) {
   const [showEventModal, setShowEventModal] = useState(false);
   const [showTeams, setShowTeams] = useState(false);
   const [name, setName] = useState("");
+  const [searchString, setSearchString] = useState("");
   const [players, setPlayers] = useState([]);
+  const [teams, setTeams] = useState([]);
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
@@ -15,7 +17,24 @@ function EventEntry({ gevent, getEventsData }) {
       );
 
       const parseResponse = await response.json();
+
       setPlayers(parseResponse);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const onSubmitTeamsForm = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVERURL}/teams/?name=${searchString}`
+      );
+
+      const parseResponse = await response.json();
+
+      console.log(parseResponse);
+      setTeams(parseResponse);
     } catch (err) {
       console.error(err.message);
     }
@@ -39,39 +58,85 @@ function EventEntry({ gevent, getEventsData }) {
         </div>
         {showTeams && (
           <div className="eventteams">
-            <h3>expanded event details</h3>
-            <h3>TEAMS</h3>
+            <h3>Expanded event details</h3>
+            <h3>TEAM - RANKINGS</h3>
 
-            <button className="my-3">Search for player</button>
-            <div>
-              <h1>Players list</h1>
-              <form onSubmit={onSubmitForm}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Enter players name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-                <button>Submit</button>
-              </form>
-              <table>
-                <thead>
-                  <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {players.map((player) => (
-                    <tr key={player.player_id}>
-                      <td>{player.player_firstname}</td>
-                      <td>{player.player_lastname}</td>
+            <div className="bg-gray-200 my-5">
+              <button className="my-3">Add player/team to event</button>
+              <div>
+                <h1>Search for players</h1>
+                <form onSubmit={onSubmitForm}>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter players name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></input>
+                  <button>Submit</button>
+                </form>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th></th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-              {players.length === 0 && <p>No results found</p>}
+                  </thead>
+                  <tbody>
+                    {players.map((player) => (
+                      <tr key={player.player_id}>
+                        <td>{player.player_firstname}</td>
+                        <td>{player.player_lastname}</td>
+                        <td>Add player to event</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                {players.length === 0 && <p>No players found</p>}
+              </div>
+            </div>
+
+            <div className="bg-gray-200 my-5">
+              <button className="my-3">Add team to event</button>
+              <div>
+                <h1>Search for teams</h1>
+                <form onSubmit={onSubmitTeamsForm}>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter players name"
+                    value={searchString}
+                    onChange={(e) => setSearchString(e.target.value)}
+                  ></input>
+                  <button>Submit</button>
+                </form>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Team ID</th>
+                      <th>Players 1 Name</th>
+                      <th>Players 2 Name</th>
+                      <th>action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {teams.map((team) => (
+                      <tr key={team.team_id}>
+                        <td>{team.team_id}</td>
+                        <td>
+                          {team.player1_firstname} {team.player1_lastname}
+                        </td>
+                        <td>
+                          {team.player2_firstname} {team.player2_lastname}
+                        </td>
+                        <td>Add team to event</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                {teams.length === 0 && <p>No teams found</p>}
+              </div>
             </div>
           </div>
         )}
