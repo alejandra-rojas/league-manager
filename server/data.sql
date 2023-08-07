72ca07498a78365d23ee4a3c930d33982dd6e64b
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-07 15:39:47 +0200

    Improved schema and sql queries

--- server/data.sql
+++ server/data.sql
@@ -14,37 +14,58 @@ CREATE TABLE leagues (
     starting_date VARCHAR(10),
     midway_point VARCHAR(10),
     end_date VARCHAR(10),
-    league_events INTEGER[],
     isFinished BOOLEAN DEFAULT false
 );
+
+CREATE TABLE players (
+    player_id SERIAL PRIMARY KEY,
+    player_firstname VARCHAR(40),
+    player_lastname VARCHAR(40),
+    player_phonenumber VARCHAR(14),
+    player_email VARCHAR(40));
+
+CREATE TABLE teams (
+    team_id SERIAL PRIMARY KEY,
+    player1_id INTEGER REFERENCES players(player_id),
+    player2_id INTEGER REFERENCES players(player_id), 
+    CHECK (player1_id <> player2_id)
+);
+
+-- Create a functional index for the unique constraint
+-- Preventing duplicated teams in different order
+CREATE UNIQUE INDEX unique_player_order_idx ON teams (LEAST(player1_id, player2_id), GREATEST(player1_id, player2_id));
 
 CREATE TABLE events (
     league_id INTEGER REFERENCES leagues(id),
     event_id SERIAL PRIMARY KEY,
     event_name VARCHAR(40),
-    participating_teams INTEGER[]);
-  
+    -- participating_teams INTEGER[]
+    );
 
-CREATE TABLE teams (
+CREATE TABLE event_teams (
+    event_id INTEGER REFERENCES events(event_id),
+    team_id INTEGER REFERENCES teams(team_id),
+    PRIMARY KEY (event_id, team_id));
+
+/* CREATE TABLE teams (
+    event_id INTEGER REFERENCES events(event_id)
     team_id SERIAL PRIMARY KEY,
     player1_fullname VARCHAR(40),
     player2_fullname VARCHAR (40),
     players_emails integer ARRAY[2],
     players_phonenumbers integer ARRAY[2],
-);
+); */
 
 
-CREATE TABLE players (
-    player_email VARCHAR(255) PRIMARY KEY,
-    player_firstname VARCHAR(40),
-    player_lastname VARCHAR(40),
-    player_phonenumber INT,
-);
+
 
 
 DROP TABLE named;
 
 ('SELECT * FROM leagues WHERE user_email = $1', [user_email])
+
+ALTER TABLE players
+ALTER COLUMN player_phonenumber TYPE VARCHAR(14);
 
 
 SELECT e.event_id, e.event_name, e.participating_teams
