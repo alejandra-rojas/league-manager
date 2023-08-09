7203cb5cc5ad25a326d3978c9cd888f5d17b6174
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-09 13:55:38 +0200

    Players loader function react-router

--- client/src/pages/admin/Players.js
+++ client/src/pages/admin/Players.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
 
 function Players() {
-  return <div>Players</div>;
+  const players = useLoaderData();
+
+  return (
+    <div className="">
+      {players.map((player) => (
+        <Link to={"/"} key={player.player_id} className="flex gap-9">
+          {player.player_firstname} {player.player_lastname}
+          <p>{player.player_phonenumber}</p>
+          <p>{player.player_email}</p>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Players;
+
+// loader function
+export const playersData = async () => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_SERVERURL}/players`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
