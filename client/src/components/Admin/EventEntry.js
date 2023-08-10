b8493c9736ffb1c76234d7b99eb8a710eaf50028
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-10 16:31:33 +0200

    Added tables and modified logic

--- client/src/components/Admin/EventEntry.js
+++ client/src/components/Admin/EventEntry.js
@@ -12,6 +12,8 @@ function EventEntry({ gevent, getEventsData }) {
   const [teams, setTeams] = useState([]);
   const [eventMatchesData, setEventMatchesData] = useState(null);
 
+  console.log(eventMatchesData);
+
   const getEventTeamsData = async () => {
     try {
       const response = await fetch(
@@ -24,7 +26,7 @@ function EventEntry({ gevent, getEventsData }) {
     }
   };
   useEffect(() => getEventTeamsData, []);
-  console.log(eventTeams);
+  //console.log(eventTeams);
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
@@ -124,6 +126,7 @@ function EventEntry({ gevent, getEventsData }) {
         const newMatches = await response.json();
         console.log("New matches created:", newMatches);
         // You can update the UI or provide feedback to the user here
+        getEventMatchesData();
       } else {
         console.log("Error generating matches:", response.statusText);
         // Handle the error and provide feedback to the user
@@ -146,65 +149,35 @@ function EventEntry({ gevent, getEventsData }) {
     }
   };
   useEffect(() => getEventMatchesData, []);
+  //console.log(gevent);
 
-  console.log(gevent);
   return (
     <>
-      <div className="bg-gray-300 p-3 mb-2 ">
-        <div className="flex justify-between items-center">
-          <h3>{gevent.event_name}</h3>
-          <div className="flex gap-3">
-            <button onClick={() => setShowEventModal(true)}>Edit</button>
+      <div className="bg-gray-300 p-3 mb-2 border rounded-md">
+        <div className="flex justify-between">
+          <div className="flex gap-7 items-center">
+            <h3>{gevent.event_name}</h3>
             <button
-              onClick={() => setShowTeams((prevState) => !prevState)}
-              className="p-1 border"
+              onClick={() => setShowEventModal(true)}
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
             >
-              {showTeams ? "-" : "+"}
+              Edit
             </button>
           </div>
+
+          <button
+            onClick={() => setShowTeams((prevState) => !prevState)}
+            className="bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-1 px-3 border border-gray-500 hover:border-transparent rounded"
+          >
+            {showTeams ? "close event" : "expand event"}
+          </button>
         </div>
 
         {showTeams && (
           <div className="eventteams">
-            <>
-              <div>
-                {/* <div className="bg-gray-200 my-5">
-            <button className="my-3">Add player to event</button>
-            <div>
-              <h1>Search for existing players</h1>
-              <form onSubmit={onSubmitForm}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Enter players name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-                <button>Submit</button>
-              </form>
-              <table>
-                <thead>
-                  <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {players.map((player) => (
-                    <tr key={player.player_id}>
-                      <td>{player.player_firstname}</td>
-                      <td>{player.player_lastname}</td>
-                      <td>Add player to event</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              {players.length === 0 && <p>No players found</p>}
-            </div>
-          </div> */}
-
-                <div className="bg-gray-200 my-5">
+            {eventMatchesData.length === 0 && (
+              <>
+                <div className="bg-gray-200 p-3 my-3 border rounded-md">
                   <button className="my-3">Add team to event</button>
                   <div>
                     <h1>Search for existing teams</h1>
@@ -219,7 +192,7 @@ function EventEntry({ gevent, getEventsData }) {
                       <button>Submit</button>
                     </form>
                     <table>
-                      <thead>
+                      <thead className="hidden">
                         <tr>
                           <th>Team ID</th>
                           <th>Players 1 Name</th>
@@ -229,7 +202,7 @@ function EventEntry({ gevent, getEventsData }) {
                       </thead>
                       <tbody>
                         {teams.map((team) => (
-                          <tr key={team.team_id}>
+                          <tr key={team.team_id} className="flex g-5">
                             <td>{team.team_id}</td>
                             <td>
                               {team.player1_firstname} {team.player1_lastname}
@@ -246,50 +219,115 @@ function EventEntry({ gevent, getEventsData }) {
                         ))}
                       </tbody>
                     </table>
-                    {teams.length === 0 && <p>No teams found</p>}
+                    {/* {teams.length === 0 && <p>No teams found</p>} */}
                   </div>
                 </div>
-              </div>
-              <h3>Participating teams</h3>
-              <ul>
-                {eventTeams.map((team) => (
-                  <li key={team.team_id} className="flex gap-10 py-1">
-                    <p>
-                      {team.player1_firstname} {team.player1_lastname} &{" "}
-                      {team.player2_firstname} {team.player2_lastname}
-                    </p>
-                    <button onClick={() => removeTeam(team.team_id)}>
-                      Remove team from event
-                    </button>
-                  </li>
-                ))}
-              </ul>
 
-              <button
-                onClick={handleGenerateMatches}
-                className="bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-3 rounded-full"
-              >
-                Create division matches table
-              </button>
-            </>
-            {eventMatchesData && (
-              <div>
-                <h1>{gevent.event_name} matches</h1>
-                The matches for this event has been published and the
-                participating teams cannot be altered anymore. If a team has
-                withdrawn update every match accordingly. For other major
-                changes to the events table, the whole event has to be deleted.
-                {console.log(eventMatchesData)}
+                <h3>Participating teams</h3>
                 <ul>
-                  {eventMatchesData.map((match) => (
-                    <li className="flex gap-10 py-1" key={match.match_id}>
+                  {eventTeams.map((team) => (
+                    <li key={team.team_id} className="flex gap-10 py-1">
                       <p>
-                        {match.team1_id} VS {match.team2_id}
+                        {team.player1_firstname} {team.player1_lastname} &{" "}
+                        {team.player2_firstname} {team.player2_lastname}
                       </p>
-                      <button>Edit match details</button>
+                      <button onClick={() => removeTeam(team.team_id)}>
+                        Remove team from event
+                      </button>
                     </li>
                   ))}
                 </ul>
+
+                <button
+                  onClick={handleGenerateMatches}
+                  className="bg-blue-300 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-3 rounded-full"
+                >
+                  Create division matches table
+                </button>
+              </>
+            )}
+            {eventMatchesData.length !== 0 && (
+              <div>
+                <div className="my-5">
+                  <h3>Participating teams</h3>
+                  <table class="table-auto border-collapse border">
+                    <thead>
+                      <tr>
+                        <th className="border-b border-solid  border-slate-600">
+                          Name
+                        </th>
+                        <th className="border-b border-solid  border-slate-600">
+                          Played
+                        </th>
+                        <th className="border-b border-solid  border-slate-600">
+                          Won
+                        </th>
+                        <th className="border-b border-solid  border-slate-600">
+                          Lost
+                        </th>
+                        <th className="border-b border-solid  border-slate-600">
+                          Bonus
+                        </th>
+                        <th className="border-b border-solid  border-slate-600">
+                          Total
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {eventTeams.map((team) => (
+                        <tr
+                          key={team.team_id}
+                          className=" border-b-orange-500 border-solid"
+                        >
+                          <td>
+                            {team.player1_firstname} {team.player1_lastname} &{" "}
+                            {team.player2_firstname} {team.player2_lastname}
+                          </td>
+                          <td>2/5</td>
+                          <td>1</td>
+                          <td>1</td>
+                          <td>0</td>
+                          <td>2</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <p>
+                  The standings table for this event has been published and the
+                  participating teams cannot be altered anymore. If a team has
+                  withdrawn update every match accordingly.
+                </p>
+                {/* {console.log(eventMatchesData)} */}
+
+                <ul className="my-5">
+                  <h1>{gevent.event_name} matches</h1>
+                  {eventMatchesData.map((match) => (
+                    <li
+                      className="flex gap-10 py-1 border"
+                      key={match.match_id}
+                    >
+                      <div>
+                        <p>
+                          {match.team1_player1_firstname} &{" "}
+                          {match.team1_player2_firstname} VS{" "}
+                          {match.team2_player1_firstname} &{" "}
+                          {match.team2_player2_firstname}
+                        </p>
+                      </div>
+                      <div>Match Date {match.match_date}</div>
+                      <div>Is match finished {match.isFinished}</div>
+                      <div>Who won {match.winner_id}</div>
+                      <div>Number of sets team 1 won {match.team1_sets}</div>
+                      <div>Number of sets team 2 won {match.team2_sets}</div>
+                      <div>Winner Score {match.winner_score}</div>
+                      <div>Team withdrawal {match.withdrawal}</div>
+                      <button>Edit results</button>
+                    </li>
+                  ))}
+                </ul>
+
+                <p>If a team has withdrawn from this table.</p>
               </div>
             )}
           </div>
