cb53d424b463272ae2417e0fc051d451f4a43561
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-10 16:32:12 +0200

    CSS minor changes

--- client/src/components/Admin/LeagueEntry.js
+++ client/src/components/Admin/LeagueEntry.js
@@ -38,16 +38,14 @@ export default function LeagueEntry({ league, getData, message }) {
 
   return (
     <div className="bg-gray-100 px-6 pt-10 pb-8 shadow-s ring-1 ring-gray-900/5  sm:rounded-lg sm:px-10">
-      <div className="flex justify-between">
+      <div className="flex gap-7 items-center">
         <h2>{league.league_name}</h2>
-        <div className="flex gap-2">
-          <button
-            onClick={() => setShowModal(true)}
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
-          >
-            Edit
-          </button>
-        </div>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
+        >
+          Edit
+        </button>
       </div>
       <div className="pt-4 pb-6 border-solid border-black-20 border-b-2 mb-6">
         <p>
@@ -81,7 +79,7 @@ export default function LeagueEntry({ league, getData, message }) {
             onClick={() => setShowEventModal(true)}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-3 rounded-full"
           >
-            Add group to {lowercaseTitle} league
+            Add event or group to {lowercaseTitle}
           </button>
         </>
       )}
