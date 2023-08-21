e41fe4672994e50efe02f0ddbe61d2e3b93bb89e
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-21 19:08:26 +0200

    a11y: Refactored breadcrumbs component

--- client/src/components/Breadcrumbs.js
+++ client/src/components/Breadcrumbs.js
@@ -1,26 +1,43 @@
+import { Fragment } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Breadcrumbs() {
   const location = useLocation();
 
-  let currentLink = "";
   const crumbs = location.pathname
     .split("/")
     .filter((crumb) => crumb !== "")
-    .map((crumb) => {
-      currentLink = +`/${crumb}`;
+    .map((crumb, index, array) => {
+      const currentLink = array.slice(0, index + 1).join("/");
+      const isLast = index === array.length - 1;
 
       return (
-        <div
-          key={crumb}
-          className="after:content-['>'] after:last-child:content-['']"
-        >
-          <Link to={currentLink}>{crumb}</Link>
-        </div>
+        <li key={crumb} className={isLast ? "font-bold underline" : ""}>
+          <Link
+            to={currentLink}
+            aria-current={isLast ? "page" : undefined}
+            aria-label={
+              isLast ? `Current page: ${crumb}` : `Navigate to: ${crumb}`
+            }
+          >
+            {crumb}
+          </Link>
+        </li>
       );
     });
 
-  return <div className="flex gap-6 ">{crumbs}</div>;
+  return (
+    <nav aria-label="Breadcrumb" className="breadcrumbs">
+      <ol className="flex gap-1 list-none p-0">
+        {crumbs.map((crumb, index) => (
+          <Fragment key={index}>
+            {index > 0 && <span className="separator">{">"}</span>}
+            {crumb}
+          </Fragment>
+        ))}
+      </ol>
+    </nav>
+  );
 }
 
 export default Breadcrumbs;
