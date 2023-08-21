7004558b84e28dd0fb7e9701d0f8a586549f5a4b
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-21 19:09:08 +0200

    a11y improvements + error handling in the form

--- client/src/components/Admin/Auth.js
+++ client/src/components/Admin/Auth.js
@@ -9,6 +9,7 @@ function Auth() {
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [isFormComplete, setIsFormComplete] = useState(false);
 
   const viewLogin = (status) => {
     setError(null);
@@ -17,6 +18,11 @@ function Auth() {
 
   const handleSubmit = async (e, endpoint) => {
     e.preventDefault();
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
     if (!isLogIn && password !== confirmPassword) {
       setError("Make sure passwords match!");
       return;
@@ -44,63 +50,85 @@ function Auth() {
   };
 
   return (
-    <div>
-      <div className="my-5 mx-5 py-5 px-5 rounded-2xl border border-gray-200">
-        <form>
-          <div className="flex justify-between">
-            {isLogIn ? "Admin login" : "Admin sign up"}
-          </div>
-          <input
-            type="email"
-            placeholder="email"
-            className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isLogIn && (
+    <section className="my-5 mx-5 py-5 px-5 rounded-2xl border border-gray-200">
+      <hgroup>
+        <h2>{isLogIn ? "Admin login" : "Admin sign up"}</h2>
+        <span>
+          {isLogIn
+            ? "Use your email & password. If you have problems contact the system administrator"
+            : "Create an admin account"}
+        </span>
+      </hgroup>
+      <form id="admin-login-form">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          required
+          placeholder="email"
+          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setIsFormComplete(e.target.value && password);
+          }}
+          autoComplete="email"
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          placeholder="password"
+          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setIsFormComplete(email && e.target.value);
+          }}
+          autoComplete="password"
+        />
+        {!isLogIn && (
+          <>
+            <label htmlFor="confirm-password">Confirm Password:</label>
             <input
               type="password"
               placeholder="confirm password"
               className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
-          )}
-          <input
-            type="submit"
-            className={
-              "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
-            }
-            onClick={(e) => {
-              handleSubmit(e, isLogIn ? "login" : "signup");
-            }}
-          />
-          {error && <p className="text-red-500">{error}</p>}
-        </form>
-        <div className="flex justify-end">
-          {isLogIn && (
-            <button
-              className={` hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded-l `}
-              onClick={() => viewLogin(false)}
-            >
-              Sign Up
-            </button>
-          )}
-          {!isLogIn && (
-            <button
-              className={` hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 `}
-              onClick={() => viewLogin(true)}
-            >
-              Admin Login
-            </button>
-          )}
-        </div>
+          </>
+        )}
+        <input
+          type="submit"
+          disabled={!isFormComplete}
+          className={
+            "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
+          }
+          onClick={(e) => {
+            handleSubmit(e, isLogIn ? "login" : "signup");
+          }}
+        />
+        {error && <p className="text-red-500">{error}</p>}
+      </form>
+      <div className="flex justify-end">
+        {isLogIn && (
+          <button
+            className={` hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 rounded-l `}
+            onClick={() => viewLogin(false)}
+            aria-label="Switch to Amin Sign Up"
+            aria-pressed={!isLogIn}
+          >
+            Sign Up
+          </button>
+        )}
+        {!isLogIn && (
+          <button
+            className={` hover:bg-gray-400 text-gray-800 font-bold py-1 px-2 `}
+            onClick={() => viewLogin(true)}
+            aria-label="Switch to Admin Login"
+            aria-pressed={isLogIn}
+          >
+            Admin Login
+          </button>
+        )}
       </div>
-    </div>
+    </section>
   );
 }
 
