a5964288f368149c9e5a6fa2016a79a9fef3d665
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-08-28 19:47:20 +0200

    SCSS: Leagues Styling

--- client/src/pages/admin/Leagues.js
+++ client/src/pages/admin/Leagues.js
@@ -1,11 +1,14 @@
+import "../../styles/Admin/Leagues.scss";
 import { useEffect, useState } from "react";
 import LeagueEntry from "../../components/Admin/LeagueEntry";
 import { useTitle } from "../../App";
 //import LeagueModal from "../../components/Admin/LeagueModal";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import LeagueModal from "../../components/Admin/LeagueModal";
 
 function Leagues() {
   useTitle("Current Leagues");
-  //const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [leagues, setLeagues] = useState(null);
   const today = new Date();
 
@@ -37,20 +40,35 @@ function Leagues() {
 
   return (
     <>
-      <div>
-        <h3>Current Leagues</h3>
-        <p>
-          Leagues that are not finished and todays date is within the range of
-          start/end date
-        </p>
-      </div>
+      <button onClick={() => setShowModal(true)} className="create-league">
+        <PlusCircleIcon width={35} />
+        <span>
+          New <span className="">league</span>
+        </span>
+      </button>
+      <section id="current-leagues">
+        <header id="league-section-header">
+          <h2 className="sr-only">Current Leagues</h2>
+          <p>
+            Leagues that are not finished and todays date is within the range of
+            start/end date
+          </p>
+        </header>
 
-      <section id="current-leagues" className="flex flex-col gap-20">
-        <ul className="flex flex-col gap-5">
-          {sortedLeagues?.map((league) => (
-            <LeagueEntry key={league.id} league={league} getData={getData} />
-          ))}
-        </ul>
+        <section id="current-leagues-data">
+          <ul>
+            {sortedLeagues?.map((league) => (
+              <LeagueEntry key={league.id} league={league} getData={getData} />
+            ))}
+          </ul>
+        </section>
+        {showModal && (
+          <LeagueModal
+            mode={"create"}
+            setShowModal={setShowModal}
+            getData={getData}
+          />
+        )}
       </section>
     </>
   );
