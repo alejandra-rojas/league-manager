b46e3844b6649812be844622c677769b032c087c
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-09-03 21:51:01 +0200

    Relocating header from Layout to Component

--- client/src/layouts/PlayersLayout.js
+++ client/src/layouts/PlayersLayout.js
@@ -1,14 +1,10 @@
 import "../styles/Admin/PlayersPage.scss";
-import { UserPlusIcon } from "@heroicons/react/24/outline";
-
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useTitle } from "../App";
-import { useEffect, useState } from "react";
-import PlayerModal from "../components/Admin/PlayerModal";
 
 export default function PlayersLayout() {
   useTitle("Players Admin");
-  const [players, setPlayers] = useState(null);
+  /*   const [players, setPlayers] = useState(null);
   const [showPlayerModal, setShowPlayerModal] = useState(false);
 
   //Getting players data
@@ -25,36 +21,11 @@ export default function PlayersLayout() {
   };
 
   useEffect(() => getPlayersData, []);
-  console.log(players);
+  //console.log(players); */
 
   return (
     <>
       <section id="players-page">
-        <header>
-          <div>
-            <h2>Players Database</h2>
-            <p>
-              Manage your players database and access individual player pages
-            </p>
-          </div>
-          {!showPlayerModal && (
-            <button
-              onClick={() => setShowPlayerModal(true)}
-              aria-label="Add Player"
-              className="create-participant"
-            >
-              <UserPlusIcon width={30} />
-              Add Player
-            </button>
-          )}
-        </header>
-        {showPlayerModal && (
-          <PlayerModal
-            mode="create"
-            getPlayersData={getPlayersData}
-            setShowPlayerModal={setShowPlayerModal}
-          />
-        )}
         <Outlet />
       </section>
     </>
--- client/src/pages/admin/Players.js
+++ client/src/pages/admin/Players.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { UserPlusIcon } from "@heroicons/react/24/outline";
 import { FunnelIcon } from "@heroicons/react/24/outline";
 import PlayerModal from "../../components/Admin/PlayerModal";
 import PlayerEntry from "../../components/Admin/PlayerEntry";
@@ -28,61 +29,86 @@ function Players() {
   console.log(players);
 
   return (
-    <div className="database-list">
-      <div className="filter-bar">
-        <label htmlFor="searchPlayer">
-          <FunnelIcon width={35} />
-          <span className="sr-only">Filter players by name</span>
-        </label>
-        <input
-          id="searchPlayer"
-          onChange={(e) => setSearchState(e.target.value)}
-          className="input-text"
-          type="text"
-          name="player"
-          value={searchState}
-          placeholder="Filter list by player's name"
-          aria-label="Filter players by name"
-        />
-      </div>
-
-      <ul className="header">
-        <li className="individual-entry">
-          <p>Name</p>
-          <p>Contact number</p>
-          <p>Email address</p>
-        </li>
-      </ul>
-
-      {!searchState && (
-        //List of all players
-        <ul className="all-participants">
-          {players?.map((player, index) => (
-            <li
-              key={player.player_id}
-              className={`${index % 2 === 0 ? "even-row" : "odd-row"}`}
-            >
-              <PlayerEntry
-                player={player}
-                getPlayersData={getPlayersData}
-                setShowPlayerModal={setShowPlayerModal}
-              />
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {searchState.length > 0 && (
-        //Results from the search
-        <PlayerSearchBar
-          players={players}
-          searchState={searchState}
-          setSearchState={setSearchState}
+    <>
+      <header>
+        <div>
+          <h2>Players Database</h2>
+          <p>Manage your players database and access individual player pages</p>
+        </div>
+        {!showPlayerModal && (
+          <button
+            onClick={() => setShowPlayerModal(true)}
+            aria-label="Add Player"
+            className="create-participant"
+          >
+            <UserPlusIcon width={30} />
+            Add Player
+          </button>
+        )}
+      </header>
+      {showPlayerModal && (
+        <PlayerModal
+          mode="create"
           getPlayersData={getPlayersData}
           setShowPlayerModal={setShowPlayerModal}
         />
       )}
-    </div>
+      <div className="database-list">
+        <div className="filter-bar">
+          <label htmlFor="searchPlayer">
+            <FunnelIcon width={35} />
+            <span className="sr-only">Filter players by name</span>
+          </label>
+          <input
+            id="searchPlayer"
+            onChange={(e) => setSearchState(e.target.value)}
+            className="input-text"
+            type="text"
+            name="player"
+            value={searchState}
+            placeholder="Filter list by player's name"
+            aria-label="Filter players by name"
+          />
+        </div>
+
+        <ul className="header">
+          <li className="individual-entry">
+            <p>Name</p>
+            <p>Contact number</p>
+            <p>Email address</p>
+          </li>
+        </ul>
+
+        {!searchState && (
+          //List of all players
+          <ul className="all-participants">
+            {players?.map((player, index) => (
+              <li
+                key={player.player_id}
+                className={`${index % 2 === 0 ? "even-row" : "odd-row"}`}
+              >
+                <PlayerEntry
+                  player={player}
+                  getPlayersData={getPlayersData}
+                  setShowPlayerModal={setShowPlayerModal}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+
+        {searchState.length > 0 && (
+          //Results from the search
+          <PlayerSearchBar
+            players={players}
+            searchState={searchState}
+            setSearchState={setSearchState}
+            getPlayersData={getPlayersData}
+            setShowPlayerModal={setShowPlayerModal}
+          />
+        )}
+      </div>
+    </>
   );
 }
 
