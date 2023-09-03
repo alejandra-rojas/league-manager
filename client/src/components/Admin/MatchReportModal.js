0908f288c87adaa9784d16050a61453730631927
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-09-03 21:55:27 +0200

    Modal styling

--- client/src/components/Admin/MatchReportModal.js
+++ client/src/components/Admin/MatchReportModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 function MatchReportModal({
   match,
@@ -49,112 +50,123 @@ function MatchReportModal({
   };
 
   return (
-    <section className="h-auto bg-white px-10 py-10 rounded-xl shadow-xl ring-1 ring-gray-900/5">
-      <header className="flex justify-between pb-3">
-        <div>
-          <h2>Match report</h2>
-          <h2>
-            Player 1: {match.team1_player1_firstname}{" "}
-            {match.team1_player1_lastname} & {match.team1_player2_firstname}{" "}
-            {match.team1_player2_lastname}
-          </h2>
-          <h2>
-            Player 2: {match.team2_player1_firstname}{" "}
-            {match.team2_player1_lastname} & {match.team2_player2_firstname}{" "}
-            {match.team2_player2_lastname}
-          </h2>
-        </div>
+    <section id="report-modal">
+      <header className="control">
+        <h5>Match report</h5>
+
         <button
           onClick={() => {
             setShowMatchReportModal(false);
           }}
         >
-          Close
+          <XMarkIcon width={25} />
+          <span>close</span>
         </button>
       </header>
 
-      <form className="flex-col">
-        <label htmlFor="matchDate">Date of the match:</label>
-        <input
-          id="matchDate"
-          required
-          type="date"
-          name="match_date"
-          value={data.match_date === null ? "" : data.match_date}
-          onChange={handleChange}
-          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-        />
-        <br />
-        <label htmlFor="isFinished">Has the match been completed:</label>
-        <input
-          id="isFinished"
-          type="checkbox"
-          name="isfinished"
-          checked={data.isfinished}
-          onChange={handleChange}
-        />
-        <br />
+      <div className="players">
+        <h6>
+          T1: {match.team1_player1_firstname} {match.team1_player1_lastname} &{" "}
+          {match.team1_player2_firstname} {match.team1_player2_lastname}
+        </h6>
+        <h6>
+          T2: {match.team2_player1_firstname} {match.team2_player1_lastname} &{" "}
+          {match.team2_player2_firstname} {match.team2_player2_lastname}
+        </h6>
+      </div>
 
-        {data.isfinished && (
-          <>
-            <label htmlFor="winner">Who won:</label>
-            <select
-              id="winner"
-              name="winner_id"
-              value={data.winner_id !== null ? data.winner_id : ""}
+      <form>
+        <div className="grouped-inputs">
+          <div className="input">
+            <label htmlFor="matchDate">Date of the match:</label>
+            <input
+              id="matchDate"
+              required
+              type="date"
+              name="match_date"
+              value={data.match_date === null ? "" : data.match_date}
               onChange={handleChange}
-              className="my-3 mx-0 py-2 px-3 rounded-xl border border-gray-200"
-            >
-              <option value="">Select a winner</option>
-              <option value={match.team1_id}>
-                {match.team1_player1_firstname} {match.team1_player1_lastname} &{" "}
-                {match.team1_player2_firstname} {match.team1_player2_lastname}
-              </option>
-              <option value={match.team2_id}>
-                {match.team2_player1_firstname} {match.team2_player1_lastname} &{" "}
-                {match.team2_player2_firstname} {match.team2_player2_lastname}
-              </option>
-            </select>
-          </>
-        )}
-        <br />
-        <label htmlFor="t1sets">Sets won by team 1:</label>
-        <input
-          id="t1sets"
-          maxLength={1}
-          placeholder="ex: 0"
-          name="team1_sets"
-          value={data.team1_sets}
-          onChange={handleChange}
-          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-        />
-        <br />
-        <label htmlFor="t2sets">Sets won by team 2:</label>
-        <input
-          id="t2sets"
-          maxLength={1}
-          placeholder="ex: 2"
-          name="team2_sets"
-          value={data.team2_sets}
-          onChange={handleChange}
-          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-        />
-        <br />
-        <label htmlFor="finalscore">Winner Score</label>
-        <input
-          id="finalscore"
-          maxLength={15}
-          placeholder="ex: '7/5 2/6 6/1"
-          name="winner_score"
-          value={data.winner_score === 0 ? "missing" : data.winner_score}
-          onChange={handleChange}
-          className="my-3 mx-0 py-3 px-4 rounded-xl border border-gray-200"
-        />
-        <br />
+            />
+          </div>
+
+          <div className="input checkbox">
+            <label htmlFor="isFinished">Match completed?</label>
+            <input
+              id="isFinished"
+              type="checkbox"
+              name="isfinished"
+              checked={data.isfinished}
+              onChange={handleChange}
+            />
+          </div>
+
+          {data.isfinished && (
+            <>
+              <div className="input">
+                <label htmlFor="winner">Who won:</label>
+                <select
+                  id="winner"
+                  name="winner_id"
+                  value={data.winner_id !== null ? data.winner_id : ""}
+                  onChange={handleChange}
+                >
+                  <option value="">Select a winner</option>
+                  <option value={match.team1_id}>
+                    {match.team1_player1_firstname}{" "}
+                    {match.team1_player1_lastname} &{" "}
+                    {match.team1_player2_firstname}{" "}
+                    {match.team1_player2_lastname}
+                  </option>
+                  <option value={match.team2_id}>
+                    {match.team2_player1_firstname}{" "}
+                    {match.team2_player1_lastname} &{" "}
+                    {match.team2_player2_firstname}{" "}
+                    {match.team2_player2_lastname}
+                  </option>
+                </select>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="grouped-inputs">
+          <div className="input">
+            <label htmlFor="finalscore">Winner score:</label>
+            <input
+              id="finalscore"
+              maxLength={15}
+              placeholder="ex: '7/5 2/6 6/1"
+              name="winner_score"
+              value={data.winner_score === 0 ? "missing" : data.winner_score}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input">
+            <label htmlFor="t1sets">Sets won by T1:</label>
+            <input
+              id="t1sets"
+              maxLength={1}
+              placeholder="ex: 0"
+              name="team1_sets"
+              value={data.team1_sets}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input">
+            <label htmlFor="t2sets">Sets won by T2:</label>
+            <input
+              id="t2sets"
+              maxLength={1}
+              placeholder="ex: 2"
+              name="team2_sets"
+              value={data.team2_sets}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
         <button
-          className={
-            "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
-          }
           type="submit"
           onClick={updateMatchData}
           aria-label="Update Match data"
